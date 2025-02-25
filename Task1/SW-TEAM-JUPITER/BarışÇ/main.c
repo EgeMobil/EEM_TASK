043aa793d71e269a32feb65fc8b4730f78b9cee1
Barış Çakır <8ariscakir@gmail.com>
2025-02-26 00:47:15 +0300

    v1.0.1 - fix: Added shutdown procedure for invalid states <b.cakir>

--- Task1/SW-TEAM-JUPITER/BarışÇ/main.c
+++ Task1/SW-TEAM-JUPITER/BarışÇ/main.c
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <stdio.h>
 #include "unistd.h"
 
@@ -78,6 +70,7 @@ void main(void)
             break;
         default:
             printf("Wrong State, Everythin will set OFF.\n");
+            shutdown();
             return;
             break;
         }
