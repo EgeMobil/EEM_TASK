7118f22787fe259f9a4b791467839d8959ed6978
yhsyn0 <43849766+yhsyn0@users.noreply.github.com>
2025-03-08 17:54:55 +0300

    Task 2 Upload

---
+++ Task2/SW-TEAM-NEPTUNE/Hüseyin/inc/main.h
@@ -0,0 +1,8 @@
+#ifndef __MAIN_H
+#define __MAIN_H    
+
+#include "tempSensor.h"
+
+void *temperatureThread(void*);
+
+#endif
---
+++ Task2/SW-TEAM-NEPTUNE/Hüseyin/src/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+
+#include "main.h"
+
+int main(void) {
+
+    pthread_t tempThread;
+    if (pthread_create(&tempThread, NULL, temperatureThread, NULL) != 0) {
+        printf("Thread can't created!\n");
+        return -1;
+    }
+
+    while (1) {
+        add2Buffer();
+        float avgTemp = getAvgTemp();
+
+        printf("\rTemperature: %.2f°C\t|\tAverage: %.2f°C   ", getTemp(), avgTemp);
+        fflush(stdout);
+        
+        msleep(1000);
+    }
+
+    pthread_join(tempThread, NULL);
+
+    return 0;
+}
+
+void *temperatureThread(void*) {
+    updateTemp();
+    return NULL;
+}
