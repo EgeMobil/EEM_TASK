6d9ea9723b0d0bb2de49383f64babc67182a23cb
Yaren <113533175+yarensari@users.noreply.github.com>
2025-03-18 22:50:57 +0300

    V1.9.4 -Task_2 completed- <yarensari>

---
+++ Task2/SW-TEAM-SATURN/YarenSari/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "battery_management_public.h"
+/*
+BatteryManagementSystem (BMS) – Batarya Yönetim Sistemi
+Senaryo:
+Elektrikli bir aracın batarya yönetim sistemini (Battery Management System - BMS) geliştiriyorsun. 
+Kullanıcı bataryanın şarj seviyesini ve voltaj değerlerini görebilmeli, ancak batarya hücrelerinin durumu ve sıcaklık hesaplamaları dışarıdan erişilememeli.
+
+Tasklar:
+
+battery_management_public.h içinde Battery_getChargeLevel(), Battery_getVoltage(), Battery_getHealthStatus() gibi dışarıya açık API’leri tanımla.
+
+battery_management_private.h içinde hücre sıcaklıkları, hücre voltaj dengesi ve batarya durumu analizlerini gizle.
+
+definition.h içinde maksimum-minimum voltaj limitleri, sıcaklık eşiği ve hücre dengeleme parametrelerini tanımla.
+
+battery_management.c içinde batarya durumu izleme algoritmalarını yaz ve dış dünyaya sadece belirlenen API'ler üzerinden erişime izin ver.
+*/
+int main(int argc, char *argv[]) 
+{
+	
+	
+	
+	return 0;
+}
