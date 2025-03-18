7dfe08e37a603acfe20fc9d823cd0a0a261d7bfb
omrvrl <omrvrl7@gmail.com>
2025-03-19 00:14:59 +0300

    v1.9.6 - task updated - <ö.varol>

--- Task3/SW-TEAM-NEPTUNE/Ömer/src/main.c
+++ Task3/SW-TEAM-NEPTUNE/Ömer/src/main.c
@@ -12,12 +12,11 @@ int main(void) {
     // İlk durumda traction durumu
     printf("Initial Traction Status: %d\n", tcs->traction_getStatus());
     
-    // Motor hızı ve pedal durumlarına göre traction kontrolünü test etme
-    // Örneğin: Throttle pedalını basma
-    tcs->throttle_position = PEDAL_PRESSED;  // Throttle pedalını bastık
-    tcs->Tcs_set_traction();  // Traction kontrolünü güncelle
 
-    // Brake pedalına basıldığında traction durumu
+    tcs->throttle_position = PEDAL_PRESSED;  
+    tcs->Tcs_set_traction();  
+
+    
     tcs->brake_position = PEDAL_PRESSED;  // Brake pedalına basıldı
     tcs->Tcs_set_traction();  // Traction kontrolünü tekrar güncelle
 
