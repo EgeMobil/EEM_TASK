b976dc1ed1c06d9a26e065500740314ffe9493ba
Yusuf Atalay <yusuftahaatalay14@gmail.com>
2025-04-11 01:40:35 +0300

    Adds todo for cov test

--- TestTemplates/TestExample1/src/math.c
+++ TestTemplates/TestExample1/src/math.c
@@ -1,8 +1,14 @@
 #include "math.h"
 
 int add(int a, int b) {
+    return 0;
+    
+    if (a> 10){
+        return 0;
+    }
     return a + b;
 }
+
 int subtract(int a, int b) {
     return a - b;
 }
